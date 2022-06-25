bb7830aa0c2ca6c271a2cd2bccdce54b46ac75e1
RookieOHY <204130199@qq.com>
2022-06-25 20:43:53 +0800

    内存逃逸demo commit

--- 26_code_review/main.go
+++ 26_code_review/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Go 代码审查方法：
 	扫描的内容：
@@ -24,5 +26,48 @@ Go 代码审查方法：
 			栈内存效率大于堆内存
 	内存逃逸：
 		原本应该被存储在栈上的变量，因为一些原因转移到了堆上
+	优化技巧：
+		项目的代码要尽可能避免内存逃逸现象，才不会被GC拖累程序。
 
 */
+/*
+①内存逃逸分析demo
+	使用
+		go build -gcflags="-m -l" .\26_code_review\main.go 编译测试是否存在内存逃逸
+	结果
+		26_code_review\main.go:39:8: new(string) escapes to heap
+	结论
+		函数返回指针类型的变量，一定会发生内存逃逸。
+		被已经逃逸的指针引用的变量也会发生逃逸.
+		slice、map 和 chan，被这三种类型所引用的指针也会发生逃逸
+*/
+func main() {
+	//newString02()
+	//fmt.Println("RookieOHY")
+	m := map[int]*string{}
+	s := "RookieOHY"
+	m[0] = &s
+
+}
+func newString() *string {
+	//申请一块内存
+	s := new(string)
+	*s = "RookieOHY"
+	fmt.Printf("%T", s)  //变量s的类型
+	fmt.Printf("%T", *s) //变量s的类型
+
+	return s
+}
+
+/*
+②逃逸到堆内存的变量不能马上被回收，只能通过垃圾回收标记清除，增加了垃圾回收的压力，所以要尽可能地避免逃逸。
+让变量分配在栈内存上，这样函数返回时就可以回收资源，提升效率。
+一下demo为优化版 返回实际的变量 不会发生内存逃逸
+*/
+
+func newString02() string {
+	//申请一块内存
+	s := new(string)
+	*s = "RookieOHY"
+	return *s
+}
