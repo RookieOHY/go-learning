a3eb4c3da5e29225e03d8728c4a1cd7b6fc84c5d
RookieOHY <204130199@qq.com>
2022-06-26 19:51:33 +0800

    😁 go rpc demo ：增加json协议代码

--- 29_go_rpc/main_client.go
+++ 29_go_rpc/main_client.go
@@ -5,19 +5,44 @@ import (
 	"go-learning/29_go_rpc/server"
 	"log"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
-// main 客户端
-func main() {
-	client, err := rpc.Dial("tcp", "localhost:1234")
+// main 客户端（基于TCP和HTTP的RPC客户端）
+func main01() {
+	//client, err := rpc.Dial("tcp", "localhost:1234")
+	client, err := rpc.DialHTTP("tcp", "localhost:1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
+	//入参
 	args := server.Args{A: 7, B: 8}
 	var reply int
+	//服务端返回指针类型，也需要用指针类型接收
 	err = client.Call("MathService.Add", args, &reply)
 	if err != nil {
 		log.Fatal("MathService.Add error:", err)
 	}
+	//打印结果
+	fmt.Printf("MathService.Add: %d+%d=%d", args.A, args.B, reply)
+}
+
+/*
+	基于json的tcp客户端
+*/
+func main() {
+	client, err := jsonrpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	//入参
+	args := server.Args{A: 7, B: 8}
+	var reply int
+	//服务端返回指针类型，也需要用指针类型接收
+	err = client.Call("MathService.Add", args, &reply)
+	if err != nil {
+		log.Fatal("MathService.Add error:", err)
+	}
+	//打印结果
 	fmt.Printf("MathService.Add: %d+%d=%d", args.A, args.B, reply)
 }
--- 29_go_rpc/main_server.go
+++ 29_go_rpc/main_server.go
@@ -4,7 +4,9 @@ import (
 	"go-learning/29_go_rpc/server"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
 /*
@@ -15,14 +17,46 @@ go rpc 概念梳理：
 		(RPC经常携带者服务注册，治理，监控等)
 		使用：
 			内置的net/rpc包来实现
+	将对象注册为rpc服务端的规则：
+		服务端方法是可以导出的
+		方法的类型可导出
+		方法必须有2个参数（参数的类型可导出，要么是自建）
+		方法的返回是error
+	定义格式：
+		func (t *T) MethodName(argType T1, replyType *T2) error
+		参数argType由客户端提供；后者由服务端返回（一定是指针类型）
+
 */
-// main 服务端
-func main() {
-	//注册服务
+// main 服务端(基于TCP、HTTP协议的RPC服务端)
+func main001() {
+	//注册服务(服务名+具体的服务对象)
 	rpc.RegisterName("MathService", new(server.MathService))
-	listen, err := net.Listen("TCP", "1234")
+	//处理http
+	rpc.HandleHTTP()
+	//在端口1234开发Tcp链接
+	listen, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
-	rpc.Accept(listen)
+	//rpc.Accept(listen)
+	http.Serve(listen, nil)
+}
+
+/*
+②基于json的tcp rpc服务端
+*/
+func main() {
+	rpc.RegisterName("MathService", new(server.MathService))
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error", e)
+	}
+	for {
+		conn, error := l.Accept()
+		if error != nil {
+			log.Println("jsonrpc.Serve: accept:", error.Error())
+			return
+		}
+		go jsonrpc.ServeConn(conn)
+	}
 }
