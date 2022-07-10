cfd1c1c168e51fe56b6d1b20e32fe1a6d87712fe
RookieOHY <204130199@qq.com>
2022-07-10 09:33:30 +0800

    😀 update: go template demo about post request

--- 30_gin/main.go
+++ 30_gin/main.go
@@ -13,25 +13,111 @@ gin相关知识点：
 	最基本的go服务：
 		由gin和其他基本库来构建
 	一些方法：
-		获取参数：
-			Param("key"):获取api参数
-			DefaultQuery("key","默认值"):获取?后面的参数值；获取不到或者没有传递，设置值为默认值。
+		获取uri参数：
+			Param("key"):获取api参数(占位符)
+		获取get请求头参数：
+			DefaultQuery("key","默认值"):获取?后面的参数值；获取不到或者没有传递key，设置值为默认值。
+			Query("key"):获取?名字为key对应的值
+			GetQuery("key")：效果同上，但是该方法存在2个返回值，前者为key对应的值，后者是一个布尔值。
+		获取post请求的参数：
+			PostForm: 同get
+			DefaultPostForm：同get
+			GetPostForm: 同get
 */
+
+type userInfo struct {
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+
+/*gin的文件上传*/
 func main() {
+
+}
+
+/*gin的参数绑定(绑定表单和绑定json)*/
+func main03() {
+	router := gin.Default()
+	router.POST("/testBind", testBind)
+	router.Run(":9200")
+
+}
+
+//测试参数的绑定
+func testBind(c *gin.Context) {
+	var userInfo userInfo
+	err := c.ShouldBind(&userInfo)
+	if err == nil {
+		c.JSON(http.StatusOK, userInfo)
+	}
+}
+
+/*post请求demo*/
+func main02() {
+	router := gin.Default()
+	router.POST("/login", login)
+	router.Run(":9200")
+}
+
+func login(c *gin.Context) {
+	//方式1
+	//username := c.PostForm("username")
+	//password:=c.PostForm("password")
+	//方式2(没有username和password参数名才会使用默认值)
+	//username := c.DefaultPostForm("username", "我是username默认值")
+	//password:=c.DefaultPostForm("password","我是password默认值")
+	//方式3
+	username, _ := c.GetPostForm("username")
+	password, _ := c.GetPostForm("password")
+	data := map[string]interface{}{
+		"name": username,
+		"pwd":  password,
+		"msg":  "success",
+	}
+	c.JSON(http.StatusOK, data)
+}
+
+/*get请求demo*/
+func main01() {
 	//gin.Default()函数默认会返回一个Engine指针。（路由的创建）
 	router := gin.Default()
+
+	//为模板设置自定义函数
+	//router.SetFuncMap(template.FuncMap{
+	//	"safe": func(str string) template.HTML{
+	//		return template.HTML(str)
+	//	},
+	//})
+
 	//func (group *RouterGroup) GET(relativePath string, handlers ...HandlerFunc) IRoutes（HandlerFunc是func(*Context)的新定义。因此，下方handleType函数的入参类型应该为*Context）
 	//参数：前者为请求映射的路径、后者为匿名函数或者为函数名
 	router.GET("/", handleTypeMethod)
 	router.GET("/user/:uid", getUser)
 	router.GET("/user", getUser2)
+	//加载一些静态的资源
+	//router.LoadHTMLGlob("templates/**/*")
+	//转发到某一模板
+	//router.GET("/posts/index",handleIndex)
 	//设置服务的运行端口（默认为8080，源码可参考utils.go下的resolveAddress函数）
 	router.Run(":9200")
 }
 
+//响应静态资源
+//func handleIndex(c *gin.Context) {
+//	c.HTML(http.StatusOK,"posts/index.tmpl",gin.H{
+//		"title":"<a href='http://www.rookieohy.top'>rookieohy的博客</a>",
+//	})
+//}
+
 //模拟查询用户2
 func getUser2(c *gin.Context) {
 	name := c.DefaultQuery("name", "bird bro")
+	//name := c.Query("name")
+	//name, flag := c.GetQuery("name")
+	//	//if !flag {
+	//	//	name="查询不到"
+	//	//	fmt.Println("query param [name] not exist!")
+	//	//}
 	u2 := user{
 		Id:   "2",
 		Name: "RookieOHY02",
@@ -44,11 +130,11 @@ func getUser2(c *gin.Context) {
 	}
 }
 
-//定义用户
+//定义用户(属性名字要求大写以暴露出去)
 type user struct {
-	Id   string
-	Name string
-	Age  int
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 //模拟查询用户
